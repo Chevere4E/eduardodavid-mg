dfc2613a625374d8a155731a4960d4d2c2579806
Stackbit Code Editor <projects@stackbit.com>
2021-08-24 22:25:26 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -82,6 +82,24 @@ export default class Home extends React.Component {
                                     </div>
                                 </div>
                             </div>
+                            <div className="col-12 col-sm-12 col-md-3">
+                                <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                                    <div class="card-header">Header</div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Secondary card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-12 col-md-3">
+                                <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+                                    <div class="card-header">Header</div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Secondary card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
 
                             <div className="post-meta">
                                 Sobre mi
