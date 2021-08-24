0f751c8358809516726d2ed4051998f09e40f987
Stackbit Code Editor <projects@stackbit.com>
2021-08-24 22:49:45 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -73,33 +73,36 @@ export default class Home extends React.Component {
                                 </h2>
                             </header>
                             <div className="row">
-                            <div className="col-12 col-sm-12 col-md-6">
-                                <div class="card text-white bg-secondary mb-3">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Secondary card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <div className="col-12 col-sm-12 col-md-6">
+                                    <div class="card text-white bg-secondary mb-3">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Secondary card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12 col-sm-12 col-md-6">
+                                    <div class="card text-white bg-secondary mb-3">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Secondary card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12 col-sm-12 col-md-6">
+                                    <div class="card text-white bg-secondary mb-4">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Secondary card title</h5>
+                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-12 col-sm-12 col-md-6">
-                                <div class="card text-white bg-secondary mb-3">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Secondary card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-12 col-sm-12 col-md-6">
-                                <div class="card text-white bg-secondary mb-4">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Secondary card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                            </div>
+                            <div className="row">
+                                
                             </div>
                             
 
