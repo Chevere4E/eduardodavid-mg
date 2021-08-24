4ec8a1d6082d82049943a62851dc387ae0fef450
Stackbit Code Editor <projects@stackbit.com>
2021-08-24 23:05:53 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -101,7 +101,7 @@ export default class Home extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="row">
+                            <div className="row text-center">
                                 <img src="/images/header-me.jpg" alt="..." class="img-thumbnail" style={{'max-height':'200px'}}/>
                                 <img src="/images/header-me.jpg" alt="..." class="img-thumbnail" style={{'max-height':'200px'}}/>
                                 <img src="/images/header-me.jpg" alt="..." class="img-thumbnail" style={{'max-height':'200px'}}/>
