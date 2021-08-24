7b3ec65d5a4848acda5bd80b350d89ef836f98fe
Stackbit Code Editor <projects@stackbit.com>
2021-08-24 22:22:56 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -73,8 +73,8 @@ export default class Home extends React.Component {
                                 </h2>
                             </header>
                             
-                            div.col-12.col-sm-12.col-md-3
-                            
+                            <div>
+
                             <div className="post-meta">
                                 Sobre mi
                             </div>
