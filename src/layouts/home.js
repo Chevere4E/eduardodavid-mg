fc53832ba65ba083c1341927e93dca5e8d4f5ae9
Stackbit Code Editor <projects@stackbit.com>
2021-08-24 21:49:00 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -78,7 +78,7 @@ export default class Home extends React.Component {
                             <br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.
                             <div className="post-meta">
                                 Mis Proyectos
-                                <a name="formacion-academica"></a>
+                                <a name="mis-proyectos"></a>
                             </div>
                             <br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.<br />.
                             <div className="post-meta">
