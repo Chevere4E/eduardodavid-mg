5e94dac9005bb281721b3f65df4b689c3b6e0715
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 05:38:30 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -140,7 +140,7 @@ export default class Home extends React.Component {
                                         <div class="card-header">INOMA C.A.</div>
                                         <div class="card-body text-dark">
                                             <p class="card-text">
-                                                Desarrollo con Wordpress y Elementor de la pagian corporactiva de INOMA.<br />
+                                                Desarrollo con Wordpress y Elementor de la pagina corporactiva de INOMA.<br />
                                                 -Cumplimiento del 100% de los objetivos y funcionalidades.<br />
                                                 -Desarrollo 100% responsivo.
                                             </p>
