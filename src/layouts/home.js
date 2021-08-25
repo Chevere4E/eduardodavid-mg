239c8ab97cfef223cfbe7fe384dc50b285be7246
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 06:50:28 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -111,7 +111,7 @@ export default class Home extends React.Component {
                                     <img src="/images/florencia-1.jpeg" alt="Florencia, Italia" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
                                     <img src="/images/helsinki-1.jpeg" alt="Helsinki, Finlandia" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
                                     <img src="/images/leon-1.jpeg" alt="Leon, Mexico" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
-                                    <img src="/images/amsterdam-1.jpeg" alt="Amsterdam, Holanda" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
+                                    <img src="/images/pisa-1.jpeg" alt="Pisa, Italia" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
                                     <img src="/images/amsterdam-1.jpeg" alt="Amsterdam, Holanda" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
                                     <img src="/images/amsterdam-1.jpeg" alt="Amsterdam, Holanda" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
                                     <img src="/images/amsterdam-1.jpeg" alt="Amsterdam, Holanda" class="img-thumbnail m-1" style={{'max-height':'250px'}}/>
