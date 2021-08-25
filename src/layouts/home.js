a278335c6a0a3ecd45fd90ce3ee59d944c7bfa51
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 06:32:20 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -89,9 +89,9 @@ export default class Home extends React.Component {
                                     <p>
                                     Soy chico de 24 nacido en la ciudad de mexico, cuyos mejores amigos son de Léon,
                                     Toluca, Puerto Vallarta, con mi novia en Ocotlán, Jalisco. Como se podran imaginar
-                                    disfruto mucho de viajar y pasearme.<br />
+                                    disfruto mucho de viajar y pasearme.<br /><br />
                                     Amante del baseball, futbol (americano y soccer), la musica Punk y de la banda como
-                                    de que no, fierro!.<br />
+                                    de que no, fierro!.<br /><br />
                                     No sere el mejor fotografo pero disfruto mucho 
                                     </p>
                                 </div>
