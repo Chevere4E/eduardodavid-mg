b874ac503e2bf2aaada15566a2002a7aeefa3bc0
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 05:09:46 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -121,7 +121,7 @@ export default class Home extends React.Component {
                                     <div class="card border-info mb-3 mb-3">
                                         <div class="card-header">Sistema LabTak</div>
                                         <div class="card-body text-info">
-                                            <h5 class="card-title text-info"><a href="">[https://labtak.takflics.com/]</h5>
+                                            <h5 class="card-title text-info"><a href="">[https://labtak.takflics.com/]</a></h5>
                                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                         </div>
                                     </div>
