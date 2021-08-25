fe1c2902647fcc6e340f6dfc4d536a226438374c
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 06:27:17 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -87,7 +87,7 @@ export default class Home extends React.Component {
                             <div className="row">
                                 <div className="col-12 col-sm-12 col-md-6">
                                     <p>
-                                    
+                                    Soy chico de 24 nacido en l
                                     </p>
                                 </div>
                             </div>
