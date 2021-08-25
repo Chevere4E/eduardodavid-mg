248ceb99329691cfd241e20e97efea154439468c
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 06:22:03 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -239,7 +239,7 @@ export default class Home extends React.Component {
                                                 -Participacion en el desarrollo 65%
                                             </p>
                                             <h5 class="card-title">
-                                                <a href="https://labtak.takflics.com/home" className="text-info">
+                                                <a href="https://www.nueetulum.com/" className="text-info">
                                                 https://www.nueetulum.com/
                                                 </a>
                                             </h5>
