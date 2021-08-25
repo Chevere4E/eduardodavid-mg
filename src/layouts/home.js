253b86ce2c9eb915103bbcd5305d702999c76880
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 05:10:11 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -126,7 +126,8 @@ export default class Home extends React.Component {
                                                 [https://labtak.takflics.com/
                                                 ]</a>
                                             </h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <p class="card-text">
+                                            Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                         </div>
                                     </div>
                                 </div>
