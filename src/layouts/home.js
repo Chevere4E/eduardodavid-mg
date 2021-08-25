8569e321074e142b522f868fea51a7425b5193dd
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 07:07:41 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -63,7 +63,6 @@ export default class Home extends React.Component {
                 <div id="content" className="site-content">
                     <main id="main" className="site-main inner">
                         <div className="post-feed">
-                            <a name="sobre-mi"></a>
                             <header className="post-header">
                                <span className="post-meta">
                                     Ingeniero en Telemática
