df5933c3f361763f043fe156a45ef8c2b8a3e3b1
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 06:29:09 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -88,7 +88,7 @@ export default class Home extends React.Component {
                                 <div className="col-12 col-sm-12 col-md-6">
                                     <p>
                                     Soy chico de 24 nacido en la ciudad de mexico, cuyos mejores amigos son de Léon
-                                    , Toluca y Puerto Vallarta, con mi novia en Ocotlán, Jalisco. 
+                                    , Toluca, Puerto Vallarta, con mi novia en Ocotlán, Jalisco. 
                                     </p>
                                 </div>
                             </div>
